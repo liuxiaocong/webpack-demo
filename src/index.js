fc5f066042649154cb9c9f483bb5e436255f8298
Xiaocong Lau <12184795@qq.com>
2018-08-23 16:06:52 +0800

    Clean for second study

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import _ from 'lodash';
-import './style.css';
-import iconUrl from './icon.jpg';
-import Data from './data.xml';
 
 function component() {
   let element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  var icon = new Image();
-  icon.src = iconUrl;
-  element.appendChild(icon);
-  console.log(Data);
+  
   return element;
 }
 
