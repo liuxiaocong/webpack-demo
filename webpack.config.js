fc79691d5423f64a0ab538b3d859cc956446c4fc
Xiaocong Lau <12184795@qq.com>
2018-09-08 15:54:33 +0800

    Change to “Get Start Session”

--- webpack.config.js
+++ webpack.config.js
@@ -4,35 +4,6 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use:[
-          'file-loader'
-        ]
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use:[
-          'csv-loader'
-        ]
-      },
-      {
-        test: /\.xml$/,
-        use:[
-          'xml-loader'
-        ]
-      }
-    ],
-  },
+    path: path.resolve(__dirname, 'dist')
+  }
 };
