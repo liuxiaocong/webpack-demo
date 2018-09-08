485c4378eb52a1e495bc3447c73621c6d0e9281a
Xiaocong Lau <12184795@qq.com>
2018-09-08 15:44:29 +0800

    HMR end

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,14 @@ module.exports = {
     contentBase: './dist',
     hot: true
   },
+  module: {
+    rules:[
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
